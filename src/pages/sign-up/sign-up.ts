6da68be975502002a44120be8e390c7529cc654a
hrk21 <hammad.rizwan21@yahoo.com>
2018-02-01 16:16:18 +0500

    Forms Updated

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ActionSheetController, ToastController, Platform, LoadingController, Loading} from 'ionic-angular';
-
+import {Validators, FormBuilder, FormGroup, AbstractControl } from '@angular/forms';
 import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
 import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
-
+import { AlertController } from 'ionic-angular';
 declare var cordova: any;
 /**
  * Generated class for the SignUpPage page.
@@ -24,8 +24,54 @@ export class SignUpPage {
   lastImage1: string = null;
   loading: Loading;
 
+  submitAttempted:Boolean;
+  data : FormGroup;
+  Name:AbstractControl;
+  Email:AbstractControl;
+  CNIC:AbstractControl;
+  Phone:AbstractControl;
+  Address:AbstractControl;
+  CarRegistrationNo:AbstractControl;
+  Password:AbstractControl;
+
   constructor(public navCtrl: NavController, private camera: Camera,
-  private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController)  {
+  private transfer: Transfer, private file: File, private filePath: FilePath,
+   public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController,
+    public platform: Platform, public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder,private alertCtrl: AlertController)  {
+    this.data = this.formBuilder.group({
+      lastImage1:['',Validators.required],
+      Name: ['', Validators.required,],
+      Email: ['',Validators.compose([ Validators.required,Validators.email])],
+      CNIC: ['', Validators.compose([Validators.required,Validators.pattern('^[0-9+]{5}-[0-9+]{7}-[0-9]{1}$')])],
+      Phone: ['', Validators.compose([Validators.required,Validators.pattern('03[0-9]{9}$')])],
+      Address: ['', Validators.required],
+      Password: ['',  Validators.compose([Validators.required,Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,15}$')])],
+    });
+    this.Name=this.data.controls['Name'];
+    this.Email=this.data.controls['Email'];
+    this.CNIC=this.data.controls['CNIC'];
+    this.Phone=this.data.controls['Phone'];
+    this.Address=this.data.controls['Address'];
+    this.Password=this.data.controls['Password'];
+    this.submitAttempted=false;
+  }
+
+  logForm(){
+    this.submitAttempted=true;
+
+    if(this.lastImage1 ==null){
+      let alert = this.alertCtrl.create({
+        title: 'Profile Image Missing',
+        subTitle: 'Please upload the required image',
+        buttons: ['Dismiss']
+      });
+      
+      alert.present();
+      return;
+    }
+
+    //ALL things are now set just need to send data to the back end check for valid!!!/
   }
 
   ionViewDidLoad() {
@@ -56,6 +102,7 @@ export class SignUpPage {
     });
     actionSheet.present();
   }
+  
   public takePicture(sourceType) {
     // Create options for the Camera Dialog
     var options = {
@@ -64,7 +111,7 @@ export class SignUpPage {
       saveToPhotoAlbum: false,
       correctOrientation: true
     };
-   
+
     // Get the data of an image
     this.camera.getPicture(options).then((imagePath) => {
       // Special handling for Android library
@@ -84,6 +131,7 @@ export class SignUpPage {
       this.presentToast('Error while selecting image.');
     });
   }
+
 
   private createFileName() {
     var d = new Date(),
